6d9cd02ea91731f2fc75bb8dd713d5be9cf71a51
Leandro <leonomanf@gmail.com>
2017-12-08 14:54:12 -0200

    a

--- usrp_spectrum.py
+++ usrp_spectrum.py
@@ -298,6 +298,7 @@ def main_loop(tb, power_db_thresh, band_thresh):
         # You could write this as binary to a file.
 
         # Scanning rate
+        print m.center_freq, centerfreq
         if timestamp == 0:
             timestamp = time.time()
             centerfreq = m.center_freq
